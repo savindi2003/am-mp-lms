79f8780a0fe7e4f3da09e7818d28a5b8466ae047
savindi2003 <141651166+savindi2003@users.noreply.github.com>
2026-05-18 09:24:36 +0530

    feat: add account create button

--- src/app/(app)/admin/accounts/page.tsx
+++ src/app/(app)/admin/accounts/page.tsx
@@ -1,3 +1,4 @@
+import AccountCreateModal from "@/modules/admin/accounts/components/AccountCreateModal";
 import AccountTableView from "@/modules/admin/accounts/components/AccountTableView";
 import { getAllAccountsForAdmin } from "@/modules/admin/accounts/data/actions";
 
@@ -10,6 +11,11 @@ export default async function AdminAccountsPage() {
       <h1 className="text-3xl font-semibold mb-5">
         Accounts
       </h1>
+
+      <div className="flex justify-end">
+        <AccountCreateModal />
+      </div>
+
 
       <AccountTableView users={users} />
     </section>
